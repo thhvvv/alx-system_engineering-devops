7ac108ae0b9a290e04dd4e40ad5a1641bc3a269a
thhvvv <makausilvia964@gmail.com>
2024-04-20 01:08:16 -0700

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,15 @@
-#!/usr/bin/pup
-# install a package Flask
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Define variables for package name and version
+$package_name = 'flask'
+$package_version = '2.1.0'
+
+# Install Flask using pip3
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => "/usr/bin/pip3 install ${package_name}==${package_version}",
+  path    => '/usr/bin',
+  creates => "/usr/local/lib/python3.x/dist-packages/${package_name}-${package_version}-py3.x.egg-info",
+  require => Package['python3-pip'],
 }
