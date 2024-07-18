5a2df8a2539d897bf3d972cc4710c34a9619b87b
thhvvv <makausilvia964@gmail.com>
2024-07-18 04:44:21 -0700

    0&1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
-
+""" queries the Reddit API and returns the number of subscribers """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    """ Returns number of subscribers"""
+    reddit_url = "https://www.reddit.com/r/{}/about.json"
+    url = reddit_url.format(subreddit)
+    response = requests.get(
+            url, headers={"User-Agent": "MyBot/1.0"}, allow_redirects=False)
+    if response.status_code != 200:
         return 0
+    data = response.json()
+    return data['data']['subscribers']
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-'''
-    this module contains the function top_ten
-'''
+""" queries the Reddit API and prints the titles
+of the first 10 hot posts listed
+"""
 import requests
-from sys import argv
 
 
 def top_ten(subreddit):
-    '''
-        returns the top ten posts for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
-    try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
+    """ prints the titles pf first 10 for a subreddit """
+    reddit_url = 'https://www.reddit.com/r/{}/hot.json?limit=10'
+    url = reddit_url.format(subreddit)
+    headers = {"User-Agent": "MyBot/1.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
         print(None)
+        return
+    data = response.json()
+    posts = data['data']['children']
 
-
-if __name__ == "__main__":
-    top_ten(argv[1])
+    for post in posts:
+        print(post['data']['title'])
